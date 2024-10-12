2963c20ce6b57c3f41659f3a9215e4bc25068160
zakariabijoy <zakaria.bijoy@live.com>
2024-10-13 01:48:15 +0600

    common js modules, es modules

---
+++ index.js
@@ -0,0 +1,7 @@
+// const generateRandomNumber = require("./utils");
+
+// console.log(generateRandomNumber());
+
+import getPosts, { getPostsLength } from "./PostController.js";
+
+console.log(getPosts(), getPostsLength());
