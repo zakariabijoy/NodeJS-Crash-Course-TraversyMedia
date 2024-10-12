da0b42991878fe7558e7a893a6f5ceae8de04964
zakariabijoy <zakaria.bijoy@live.com>
2024-10-13 02:33:48 +0600

    Environment Variables & .env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import http from "http";
 
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   //   res.setHeader("Content-Type", "text/html");
